d7593cb13c1fdde578b6445a56b2584d95308c6a
JunHyun <bjsd2001a@naver.com>
2024-11-11 16:29:02 +0900

    DDL DML 제약조건

--- ddl01.sql
+++ ddl01.sql
@@ -12,4 +12,73 @@ FROM new_table;
 
 DESC new_table; -- 테이블 구조 보는 것 desc
 
-SELECT SYSDATE FROM dual;
+
+CREATE TABLE professor2 -- 교수2 생성
+AS
+SELECT * FROM professor; -- 교수2로 교수의 모든것을 복사
+
+SELECT *
+FROM professor2
+WHERE position = 'assistant professor';
+
+UPDATE professor2
+--SET bonus = 200 --200으로 저장
+SET bonus = bonus + 200 -- 200을 인상
+WHERE  position = 'assistant professor';
+
+
+commit; -- 중간 세이브 포인트 느낌
+rollback; -- 세이브 전 으로 가는 느낌
+
+CREATE TABLE dept4
+AS
+SELECT dcode,area FROM dept2;
+
+SELECT *
+FROM dept4;
+
+ALTER TABLE dept4
+ADD (location VARCHAR2(10) DEFAULT 'Cheonan');
+
+ALTER TABLE dept4
+ADD (temp_code VARCHAR2(2) DEFAULT 'N');
+
+INSERT INTO dept4
+VALUES (2000,'Cheonan');
+
+UPDATE dept4
+SET area = 'Asan'
+WHERE dcode = 2000;
+
+INSERT INTO dept4
+VALUES (2001,'Asan Office','Asan', 'Y');
+
+CREATE TABLE dept6
+AS
+SELECT * FROM dept4;
+
+CREATE TABLE dept5
+AS
+SELECT * FROM dept4;
+
+SELECT * FROM dept5;
+SELECT * FROM dept6;
+
+
+-- 테이블 삭제 (롤백 ROLLBACK 불가 / 자동 COMMIT 커밋
+DROP TABLE dept5;
+
+TRUNCATE TABLE dept5;
+TRUNCATE TABLE dept6;
+
+-- 데이터 삭제 DELETE / 롤백 ROLLBACK 가능 / 자동 COMMIT 커밋 X
+SELECT * FROM dept4;
+
+DELETE FROM dept4;
+ROLLBACK;
+
+
+SELECT *
+--DELETE 
+FROM dept4
+WHERE temp_code = 'Y';
---
+++ ddl02.sql
@@ -0,0 +1,57 @@
+-- ddl 02 제약조건
+
+-- CONSTRAINT
+CREATE TABLE tt01
+(
+    no NUMBER(3) UNIQUE , -- 중복 값이 들어가지 못한다.
+    name VARCHAR(10) NOT NULL , -- NULL이 들어가면 안된다.
+    hiredate DATE
+);
+
+INSERT INTO tt01 -- 제약 조건 확인 Shift + F4
+VALUES (1, '이름1', SYSDATE);
+
+INSERT INTO tt01
+VALUES (2, '이름2', SYSDATE);
+
+SELECT * FROM tt01;
+
+INSERT INTO tt01
+VALUES (3, '이름3', SYSDATE);
+
+CREATE TABLE tt03
+(
+    no NUMBER(3)
+        CONSTRAINT tt03_no_uk UNIQUE,
+    name VARCHAR2(10)
+        CONSTRAINT tt03_name_nn NOT NULL,
+    --score NUMBER(3) CHECK (score BETWEEN 0 AND 100) 약식
+    score NUMBER(3)
+        CONSTRAINT tt03_scoer_ck CHECK (score BETWEEN 0 AND 100),
+    pass VARCHAR2(2)
+        CONSTRAINT tt03_pass_ck CHECK (pass IN ('Y', 'N'))
+);
+
+--Oracle DB 숫자/문자/날짜   boolean이 없다 True/False 구분이 없어서 값으로 구분함
+
+-- 1:true 0:false // Y:true N:flase // T:true F:false // true:true false:falase
+
+SELECT * FROM tt03;
+
+INSERT INTO tt03
+VALUES (1,'이름1', 50, 'Y');
+
+INSERT INTO tt03
+VALUES (1,'이름1', 50, 'Y'); -- 중복 UNIQUE
+
+INSERT INTO tt03
+VALUES (1,NULL, 50, 'Y'); -- 이름 NOT NULL
+
+INSERT INTO tt03
+VALUES (1,'이름1', 500, 'Y'); -- SCORE 범위는 0~100까지
+
+INSERT INTO tt03
+VALUES (2,'이름2', 70, 'D'); -- pass Y OR N 만 가능
+
+
+
